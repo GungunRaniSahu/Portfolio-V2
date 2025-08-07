3ababe05383bc6a4c1571bbc3d6e000ca0435380
GungunRaniSahu <gungunsahu1976@gmail.com>
2025-08-07 21:01:28 +0530

    -- main files

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Skills from './components/Skills'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 dark:from-dark-300 dark:to-dark-200">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App 
